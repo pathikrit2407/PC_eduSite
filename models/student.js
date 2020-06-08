7f8d62e797ad14ec8e1aea7ee1726ecd3059a743
Gaurav Bhattacharjee <51418644+guilefoylegaurav@users.noreply.github.com>
2020-06-08 19:21:29 +0530

    Updated model and changed filename

--- models/student.js
+++ models/student.js
@@ -1,47 +1,17 @@
-const mongoose = require("mongoose");
+var mongoose = require('mongoose'); 
+var Schema = mongoose.Schema; 
 
-const studentSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please provide name of the student"],
-    trim: true,
-  },
-  username: {
-    type: String,
-    required: [true, "Please provide a username"],
-  },
-  email: {
-    type: String,
-    trim: true,
-    required: [true, "Please provide your email"],
-    lowercase: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Please provide a password"],
-  },
-  passwordConfirm: {
-    type: String,
-  },
-  contact: {
-    type: Number,
-  },
-  fatherName: {
-    type: String,
-    trim: true,
-  },
-  motherName: {
-    type: String,
-    trim: true,
-  },
-  batch: {
-    type: String,
-  },
-  performances: {
-    //
-  },
-});
 
-const Student = mongoose.model("Student", studentSchema);
+var Student = new Schema({
+	
+	name:String, 
+	contact:String, 
+	email :String, 
+	father:String, 
+	mother:String, 
 
-module.exports = Student;
+}); 
+
+
+
+module.exports = mongoose.model('Student', Student);
